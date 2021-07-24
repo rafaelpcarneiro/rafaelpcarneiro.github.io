7e3a3683ba0675873e2f4971323f0eccc2fb0185
rafaelpcarneiro <rafaelpcarneiro@gmail.com>
2021-07-24 15:15:07 -0300

    New design

--- img/convert_pdf_to_png.sh
+++ img/convert_pdf_to_png.sh
@@ -3,4 +3,5 @@ file=$1
 
 pdftoppm  $1 -jpegopt quality=100 -jpeg ${file//.pdf/}
 
-convert ${file//.pdf/-1.jpg} -transparent white -resize 200x200 ${file//pdf/png}
+#convert ${file//.pdf/-1.jpg} -transparent white -resize 1100x400 ${file//pdf/png}
+convert ${file//.pdf/-1.jpg} -quality 100 ${file//pdf/png}
